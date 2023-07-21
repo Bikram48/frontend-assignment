177bfa2811cc03013aa8cf820ee73b82e20f1c70
Bikram48 <chandbikram001@gmail.com>
2023-07-21 20:02:45 +0545

    fetch - product data from fakestoreapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { fetchProducts } from "./redux/apis";
 
 export default function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchProducts();
+    };
+
+    fetchData();
+  });
+
   return (
     <div>
       <Navbar />
