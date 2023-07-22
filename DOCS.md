9af8023e94310d03fb28a7b7bceab6ed87ed1d06
Bikram48 <chandbikram001@gmail.com>
2023-07-22 14:11:54 +0545

    add - docs.md file

---
+++ DOCS.md
@@ -0,0 +1,10 @@
+## Technology Used:
+
+- #### React JS
+- #### TailWind CSS
+- #### Redux
+
+## To Run This Project:
+
+- #### If you haven't install vite first install vite using command npm install vite
+- #### Run npm run dev
--- src/components/Products/ProductSearch.jsx
+++ src/components/Products/ProductSearch.jsx
@@ -4,20 +4,21 @@ import ProductCard from "./ProductCard";
 
 export default function ProductSearch() {
   const searchQuery = useSelector((state) => state.searchQuery);
-  //   console.log(searchQuery);
   const allProducts = useSelector((state) => state.products);
-  const searchResults =
-    allProducts &&
-    allProducts.filter((product) =>
-      product.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
 
-  console.log(searchResults);
+  const searchResults =
+    (allProducts &&
+      allProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery)
+      )) ||
+    [];
+
   const renderProduct =
     searchResults &&
     searchResults.map((product) => {
       return <ProductCard item={product} />;
     });
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {renderProduct}
